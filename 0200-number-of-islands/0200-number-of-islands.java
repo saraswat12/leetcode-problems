047eb7dd42a67612345490718fbeb6d6d44a6cd4
saraswat12 <111674832+saraswat12@users.noreply.github.com>
2024-04-03 23:36:28 +0530

    Time: 2 ms (99.68%), Space: 49.3 MB (59.54%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public boolean isValid(char[][] grid, int i, int j, int row, int col){
+        if(i >= 0 && i<row && j>=0 && j<col && grid[i][j] == '1'){
+            return true;
+        }
+        return false;
+    }
+    public void dfs(char[][] grid, int i, int j, int row, int col){
+        grid[i][j] = '0';
+        
+        if(isValid(grid, i+1, j, row, col)){
+            dfs(grid, i+1, j, row, col);
+        }
+        if(isValid(grid, i-1, j, row, col)){
+            dfs(grid, i-1, j, row, col);
+        }
+        if(isValid(grid, i, j+1, row, col)){
+            dfs(grid, i, j+1, row, col);
+        }
+        if(isValid(grid, i, j-1, row, col)){
+            dfs(grid, i, j-1, row, col);
+        }
+    }
+    public int numIslands(char[][] grid) {
+ 
+        
+    int row = grid.length;
+    int col = grid[0].length;
+      int count = 0; 
+    for(int i =0; i<row; i++){
+        for(int j =0; j<col; j++){
+            if(grid[i][j] == '1'){
+                count++;
+                dfs(grid, i, j, row, col);
+                
+            }
+        }
+    }
+        return count;
+        
+        
+    }
+}
