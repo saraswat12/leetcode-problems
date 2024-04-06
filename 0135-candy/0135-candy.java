cff23735a210570cb7295fff61b70d04906e7cc9
saraswat12 <111674832+saraswat12@users.noreply.github.com>
2024-04-06 16:45:10 +0530

    Time: 3 ms (87.80%), Space: 44.9 MB (95.32%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+    
+        
+    int []arr = new int[ratings.length];
+    Arrays.fill(arr, 1);
+        
+    for(int i = 1; i<ratings.length; i++){
+        if(ratings[i-1]< ratings[i]){
+            arr[i] = arr[i-1]+1;
+        }
+        
+    }
+        for(int i = ratings.length - 2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i] = Math.max(arr[i], arr[i+1]+1);
+            }
+        }
+        
+        int sum = 0;
+        for(int i =0; i<ratings.length; i++){
+            sum += arr[i];
+        }
+        
+                       
+              return sum; 
+        
+       
+    }
+}
+
+
