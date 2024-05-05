201f14eff792794f6fadfbcdc2517a7ccf0717ad
saraswat12 <111674832+saraswat12@users.noreply.github.com>
2024-05-05 11:30:13 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (59.91%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -5,10 +5,12 @@ class Solution {
     public void call(int[] nums, List<Integer> output, int idx, List<List<Integer>> ans){
         
         ans.add(new ArrayList<>(output));
+      
         
         for(int i = idx; i < nums.length; i++){
             output.add(nums[i]);
             call(nums, output, i + 1, ans);
+           
             output.remove(output.size() - 1);
         }
     }
