3600f4249ee6d639d62220a8070c628c04ddec8c
saraswat12 <111674832+saraswat12@users.noreply.github.com>
2024-05-23 22:55:48 +0530

    Time: 6 ms (73.34%), Space: 44.5 MB (59.45%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,52 +1,36 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        
         Stack<Integer> st = new Stack<>();
-
-        for(int i =0; i<tokens.length; i++){
-            String token = tokens[i]; // Integer.parseInt(tokens[i])
-            if(token.length() == 1 && "+-/*".contains(token)){
-            char curr = token.charAt(0);
-
-             if (st.size() < 2) {
-                    throw new IllegalArgumentException("Invalid expression");
-                }
-                int a = st.pop();
-                 int b = st.pop();
+        int n = tokens.length;
+        
+        for(int i=0; i<n; i++){
+            if(!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("*") && !tokens[i].equals("/")){
                
-            if(curr == '+'){
-                st.push(b+a);
-            }
-            else if(curr == '*'){
-                st.push(b*a);
-            }
-             else if(curr == '-'){
-                st.push(b-a);
-            }
-
-            else if(curr == '/'){
-               
-                  if (a == 0) {
-                        throw new ArithmeticException("Division by zero");
-                    }
-                st.push(b/a);
-            }
-
-            }
-            else{
-                st.push(Integer.parseInt(token));
+                st.push(Integer.parseInt(tokens[i]));
             }
             
-
+            else{
+                int b = st.pop();
+                int a = st.pop();
+                
+                if(tokens[i].equals("+")){
+                    st.push(a+b);
+                }
+                else if(tokens[i].equals("-")){
+                    st.push(a-b);
+                }
+                else if(tokens[i].equals("*")){
+                    st.push(a*b);
+                }
+                else{
+                    st.push(a/b);
+                }
+            }
+            
         }
-
-        if (st.size() != 1) {
-            throw new IllegalArgumentException("Invalid expression");
-        }
-
-        return st.pop();
-
-
-
+        
+        int ans = st.pop();
+        
+        return ans;
     }
 }
